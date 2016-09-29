2391a1ed4fdc7f619a4af84da4260e8b439c638e
Bryce Beagle <bryce.beagle@gmail.com>
2016-09-29 03:29:56 -0700

    Experiment to see effectiveness of motion tracking

--- TestFiles/BSMOG2.py
+++ TestFiles/BSMOG2.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
-video = cv2.VideoCapture(1)
+from TestFiles import colors
+
+video = cv2.VideoCapture(2)
 
 greenLower = (29,  86,   6)
 greenUpper = (64, 255, 255)
@@ -16,27 +18,29 @@ while True:
 
     fgmask = cv2.inRange(fgmask, 255, 255)
 
-    # fgmaskTemp = cv2.GaussianBlur(fgmaskTemp)
+    color = cv2.bitwise_and(frame, frame, mask=fgmask)
 
-    cv2.imshow('Mask', fgmask)
+    blue = cv2.inRange(color, *colors.blue)
+
+    # fgmask = cv2.fastNlMeansDenoising(fgmask)
+
+    cv2.imshow('Blue', blue)
 
     height, width = fgmask.shape[:2]
 
     _, contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    color = cv2.bitwise_and(frame, frame, mask=fgmask)
-
-    for contour in contours:
-
-        if cv2.contourArea(contour) > 1000:
-            ((x, y), radius) = cv2.minEnclosingCircle(contour)
-
-            moment = cv2.moments(contour)
-
-            center = (int(moment["m10"] / moment["m00"]), int(moment["m01"] / moment["m00"]))
-
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    # for contour in contours:
+    #
+    #     if cv2.contourArea(contour) > 1000:
+    #         ((x, y), radius) = cv2.minEnclosingCircle(contour)
+    #
+    #         moment = cv2.moments(contour)
+    #
+    #         center = (int(moment["m10"] / moment["m00"]), int(moment["m01"] / moment["m00"]))
+    #
+    #         cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+    #         cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Color", color)
