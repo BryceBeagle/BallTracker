7815eba898d2e0c22258897eb5118e8f19e2c278
Bryce Beagle <bryce.beagle@gmail.com>
2016-11-21 12:21:06 -0700

    Made more modular with the inclusion of find.contourCenter

--- helpers/draw.py
+++ helpers/draw.py
@@ -1,22 +1,18 @@
 import cv2
+from helpers import find
 
 
-def circlesFromContours(contours, count, frame):
+def circlesFromContours(frame, contours, count):
 
     count = min(count, len(contours))
 
-    for bearing in contours[ : count]:
+    for contour in contours[ : count]:
 
-        (x, y), radius = cv2.minEnclosingCircle(bearing)
-        moment = cv2.moments(bearing)
+        (x, y), radius = cv2.minEnclosingCircle(contour)
 
         if 10 < radius < 100:
 
-            try:
-                center = (int(moment["m10"] / moment["m00"]), int(moment["m01"] / moment["m00"]))
-            except ZeroDivisionError:
-                print("Zero area circle?")
-                continue
+            center = find.contourCenter(contour)
 
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
