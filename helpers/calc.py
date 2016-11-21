7188e3b8a9fde35c88e1d6bae0ea057144c76b6d
Bryce Beagle <bryce.beagle@gmail.com>
2016-11-21 15:16:41 -0700

    Helper file to perform simple math operations

---
+++ helpers/calc.py
@@ -0,0 +1,27 @@
+import math
+
+
+# Distance between two points
+def distance(x1, y1, x2, y2):
+
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
+# Finds average of two points
+def average(x1, y1, x2, y2):
+
+    xAve = (x1 + x2) / 2
+    yAve = (y1 + y2) / 2
+
+    return xAve, yAve
+
+
+# For use with the two coordinates of the yellow markers
+def origin(x1, y1, x2, y2):
+
+    xAve, yAve = average(x1, y1, x2, y2)
+
+    xOrig = xAve + (y1 - yAve)
+    yOrig = yAve + (x1 - xAve)
+
+    return xOrig, yOrig
