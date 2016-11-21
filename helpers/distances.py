77f732839c7433f0f12dba0a21ba9f41a64c7e96
Bryce Beagle <bryce.beagle@gmail.com>
2016-11-21 11:27:49 -0700

    File to store constant lengths and distances
    Initial revision

---
+++ helpers/distances.py
@@ -0,0 +1,12 @@
+
+# Reference distance
+BETWEEN_YELLOW  = 180
+
+# Arm lengths
+ARM_LOWER       = 148
+ARM_UPPER       = 160
+
+# Joint offsets
+STAND_VERT      = 89.214
+END_VERT        = 40
+END_HORIZ       = 20.3
