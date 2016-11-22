5757be099835d73d06ade83a09f269d9187d11f8
Bryce Beagle <bryce.beagle@gmail.com>
2016-11-21 17:27:39 -0700

    Added pixelRatio() Moved contourCenter from helpers/find.py Made origin() take contours instead of points

--- helpers/calc.py
+++ helpers/calc.py
@@ -1,8 +1,11 @@
 import math
-
+from helpers import distance
 
 # Distance between two points
-def distance(x1, y1, x2, y2):
+import cv2
+
+
+def pythag(x1, y1, x2, y2):
 
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
@@ -16,8 +19,19 @@ def average(x1, y1, x2, y2):
     return xAve, yAve
 
 
-# For use with the two coordinates of the yellow markers
-def origin(x1, y1, x2, y2):
+# For use only with the two yellow marker contours
+def origin(yellowMarkers):
+
+    marker1 = contourCenter(yellowMarkers[0])
+    marker2 = contourCenter(yellowMarkers[1])
+
+    # marker1 needs to have larger y value
+    if marker2[1] < marker1[1]:
+        x1, y1 = marker1
+        x2, y2 = marker2
+    else:
+        x1, y1 = marker2
+        x2, y2 = marker1
 
     xAve, yAve = average(x1, y1, x2, y2)
 
@@ -25,3 +39,27 @@ def origin(x1, y1, x2, y2):
     yOrig = int(yAve + (xAve -   x1))
 
     return xOrig, yOrig
+
+
+def pixelRatio(yellowMarkers):
+
+    marker1 = contourCenter(yellowMarkers[0])
+    marker2 = contourCenter(yellowMarkers[1])
+
+    markerDistance = pythag(*marker1, *marker2)
+
+    return distance.BETWEEN_YELLOW / markerDistance
+
+
+# Find the center of a contour using moments
+def contourCenter(contour):
+
+    moment = cv2.moments(contour)
+
+    try:
+        center = (int(moment["m10"] / moment["m00"]), int(moment["m01"] / moment["m00"]))
+    except ZeroDivisionError:
+        print("Zero area circle?")
+        return
+
+    return center
