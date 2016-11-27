ddb5b39383115cc2e7d78111e267834d61d2649f
Bryce Beagle <bryce.beagle@gmail.com>
2016-11-27 01:30:34 -0700

    Store bearings as objects

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import cv2
+from helpers import find, draw, calc, color
 
-from helpers import color
+video = cv2.VideoCapture(1)
 
-# video = cv2.videoCapture(1)
+video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+video.set(cv2.CAP_PROP_FRAME_WIDTH,  1280)
+video.set(cv2.CAP_PROP_FRAME_HEIGHT,  720)
 
-# video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-# video.set(cv2.CAP_PROP_FRAME_WIDTH,  1280)
-# video.set(cv2.CAP_PROP_FRAME_HEIGHT,  720)
+origin = 0, 0
 
 while True:
 
-    # _, frame = video.read()
-    frame = cv2.imread('testReferencePos.png')
+    frame = video.read()[1]
+
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    blueFrame, blueMask = color.isolate(hsv, *color.BLUE_RANGE)
-    redFrame,    = color.isolate(hsv, *color.RED_RANGE)
+    yellowMarkers = find.bearingsOfColor(hsv, color.YELLOW_RANGE)
+    blueBearings  = find.bearingsOfColor(hsv, color.BLUE_RANGE)
+
+    if yellowMarkers and len(yellowMarkers) == 2:
+        origin     = calc.origin(yellowMarkers)
+        mmPerPixel = calc.pixelRatio(yellowMarkers)  # TODO: Remove duplicate call to calc.contourCenterFromMoment()
+
+    cv2.circle(frame, origin, 3, color.GREEN, 3)
+
+    if yellowMarkers:
+        frame = draw.circlesFromContours(frame, yellowMarkers, 2, minRadius=5)
+    if blueBearings:
+        frame = draw.circlesFromContours(frame, blueBearings,  6, minRadius=15)
+
+    frame = cv2.line(frame, (5, 5), (  5, 100), color.RED, 2)
+    frame = cv2.line(frame, (5, 5), (200,   5), color.RED, 2)
+
+    cv2.imshow("Frame",  frame)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27: break
 
